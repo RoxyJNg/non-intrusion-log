30a88badca71be142967ebfdd30b6849637c4b69
RoxyJNg <skyprinceton@hotmail.com>
2020-03-04 20:27:50 +0800

    Create repository

---
+++ src/main/java/com/null01/nonintrusionlog/NonIntrusionLogApplication.java
@@ -0,0 +1,13 @@
+package com.null01.nonintrusionlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NonIntrusionLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NonIntrusionLogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/null01/nonintrusionlog/NonIntrusionLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.null01.nonintrusionlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NonIntrusionLogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
